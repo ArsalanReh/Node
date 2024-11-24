b1d4e51fcc8e8d611f47e59ed2f03bcafe291037
ArsalanReh <arsalanrrehman@gmail.com>
2024-11-24 18:58:11 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const add =require("./math")
+const sub= require("./math")
+
+console.log("Hello World");
+console.log(add(3,4));
+console.log(sub(3,4));
+
+const arr=[1,2,3,4,5,6];
+console.log(arr.map((num)=>num*5));
---
+++ math.js
@@ -0,0 +1,9 @@
+const add=(a,b)=>{
+    return a+b;
+}
+module.exports=add;
+
+const sub=(c,d)=>{
+    return c-d;
+}
+module.exports=sub;
